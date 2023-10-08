5455c452b43b9433198d93284d69a0a263bb16a7
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-10-09 02:16:44 +0530

    feat: handle heavy calc via web-worker

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function Calculator() {
   const [result, setResult] = useState(null);
+  const [count, setCount] = useState(0);
 
   const calculate = () => {
     // Create a new Web Worker
@@ -21,7 +22,15 @@ function Calculator() {
   return (
     <div>
       <button onClick={calculate}>Start Calculation</button>
-      {result && <p>Result: {result}</p>}
+      {result && <h2>Result: {result}</h2>}
+      <h2>Click to increment. This feature works even during calculations.</h2>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        {count}
+      </button>
     </div>
   );
 }
